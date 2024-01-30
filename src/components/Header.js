83e146c8a1267eb713df76c51eefc7b58c9e62b5
Melissa <melissadcalderon@gmail.com>
2024-01-30 03:19:43 -0500

    task 3 updates

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Navbar, NavbarBrand, Container } from 'reactstrap'; // Added Container to the import
+import NucampLogo from './app/assets/img/logo.png';
+
+const Header = () => { // Changed {header} to ()
+    return (
+        <Navbar dark color='primary' sticky='top' expand='md'>
+            <Container>
+                <NavbarBrand href='/'>
+                    <img src={NucampLogo} alt='nucamp logo' />
+                </NavbarBrand>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Header;
