8aefcca1850703d068c8b4c6d76030f26bf8261e
Melissa <melissadcalderon@gmail.com>
2024-02-04 02:51:48 -0500

    CampsiteDetailPage with useParams

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import './App.css';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
+
 
 function App() {
     return (
@@ -15,6 +17,10 @@ function App() {
                 <Route path='/' element={<HomePage />} />
                 <Route path='contact' element={<ContactPage />} />
                 <Route path='directory' element={<CampsitesDirectoryPage />} />
+                <Route
+                    path='directory/:campsiteId'
+                    element={<CampsiteDetailPage />}
+                />
             </Routes>
             <Footer />
         </div>
--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const CampsiteCard = ({ campsite }) => {
+    const { id, image, name } = campsite;
+
     return (
-        <Card>
-            <CardImg
-                width="100%"
-                src={campsite.image}
-                alt={campsite.name}
-            />
-            <CardImgOverlay>
-                <CardTitle>{campsite.name}</CardTitle>
-            </CardImgOverlay>
-        </Card>
+        // Wrap Card component in Link component
+        <Link to={`/${id}`}>
+            <Card>
+                <CardImg
+                    width="100%"
+                    src={image}
+                    alt={name}
+                />
+                <CardImgOverlay>
+                    <CardTitle>{name}</CardTitle>
+                </CardImgOverlay>
+            </Card>
+        </Link>
     );
 };
 
 CampsiteCard.propTypes = {
     campsite: PropTypes.shape({
+        id: PropTypes.number.isRequired,
         image: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired
-    }).isRequired
+        name: PropTypes.string.isRequired,
+    }).isRequired,
 };
 
 export default CampsiteCard;
