f5ac9c5c0ca0e9915746a78e7b3c66f6b9d3b0b9
Melissa <melissadcalderon@gmail.com>
2024-02-03 23:58:53 -0500

    add in CampsitesList.js

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import { CAMPSITES } from '../../app/shared/CAMPSITES';
 import { Col, Row } from 'reactstrap';
 import CampsiteCard from "./CampsiteCard";
+import { selectAllCampsites } from './campsitesSlice';
 
-const CampsitesList = ({campsites = CAMPSITES}) => {
+const CampsitesList = () => {
+    const campsites = selectAllCampsites();
 return (
     <Row className ='ms-auto'>
 
-    {CAMPSITES.map(campsite => {
+    {campsites.map((campsite) => {
         return (
             <Col md='5' className='m-4' key={campsite.id}>
                 <CampsiteCard campsite={campsite} />
             </Col>
-        )
-    }
-    )}
-
+        );
+    })}
 
     </Row>
-    )
-}
+    );
+};
 export default CampsitesList;
