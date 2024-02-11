eff99545ac3080d6a661697dc36b0937f1d0c935
Melissa <melissadcalderon@gmail.com>
2024-02-11 14:53:44 -0500

    add commentform

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import { Col } from 'reactstrap';
 import Comment from './Comment';
+import CommentForm from './CommentForm'; // Import CommentForm
 import { selectCommentsByCampsiteId } from './commentsSlice';
+
 
 const CommentsList = ({ campsiteId }) => {
     const comments = selectCommentsByCampsiteId(campsiteId);
 
-    if (comments && comments.length > 0) {
-        return (
-            <Col md='5' className='m-1'>
-                <h4>Comments</h4>
-                {comments.map((comment) => {
-                    return <Comment key={comment.id} comment={comment} />;
-                })}
-            </Col>
-        );
-    }
     return (
         <Col md='5' className='m-1'>
-            There are no comments for this campsite yet.
+            <h4>Comments</h4>
+            {comments && comments.length > 0 ? (
+                <>
+                    {comments.map((comment) => (
+                        <Comment key={comment.id} comment={comment} />
+                    ))}
+                    <CommentForm campsiteId={campsiteId} />
+                </>
+            ) : (
+                <>
+                    There are no comments for this campsite yet.
+                    <CommentForm campsiteId={campsiteId} />
+                </>
+            )}
         </Col>
     );
 };
